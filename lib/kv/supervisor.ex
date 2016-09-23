e98e116d31a5cf9102163c7ed2788545d121c9ac
yurifrl <yurifrl@hotmail.com>
2016-09-23 16:31:34 -0300

    initial

---
+++ lib/kv/supervisor.ex
@@ -0,0 +1,17 @@
+defmodule KV.Supervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, :ok)
+  end
+
+  def init(:ok) do
+
+    children = [
+      worker(KV.Registry, [KV.Registry]),
+      supervisor(KV.Bucket.Supervisor, [])
+    ]
+
+    supervise(children, strategy: :one_for_one)
+  end
+end
